d5e30abdede889ff3cc09f654ebabe736cb74d7c
Benjamin Martin <benjamin.martin2010@gmail.com>
2017-12-03 17:33:50 -0600

    Define Disc struct

--- lib/connect_engine/coordinate.ex
+++ lib/connect_engine/coordinate.ex
@@ -10,16 +10,14 @@ defmodule ConnectEngine.Coordinate do
   @enforce_keys [:row, :col]
   @col_range 1..7
   @row_range 1..6
+
+  defstruct [:row, :col]
+
   @doc """
   Defines the Coordinate struct
 
   * :row - row coordinate (1 to 6)
   * :col - column coordinate (1 to 7)
-  """
-  defstruct [:row, :col]
-
-  @doc """
-  Defines a new Coordiante
   """
   def new(row, col) when row in(@row_range) and col in(@col_range) do
     {:ok, %Coordinate{row: row, col: col}}
